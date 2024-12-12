29459aba2e4eb5f47e1f5dbc67a99e8c4f8c6e4c
akinara666 <maksim040903@mail.ru>
2024-12-12 03:16:07 +0300

    feat: infinite recommendation scroll

--- Backend/utils/recommendations.py
+++ Backend/utils/recommendations.py
@@ -3,4 +3,4 @@ from models import Movie
 
 def get_recommendations(user_id):
     all_movie_ids = [movie.id for movie in Movie.query.all()]
-    return random.sample(all_movie_ids, min(len(all_movie_ids), 50))
+    return random.sample(all_movie_ids, min(len(all_movie_ids), 1))
