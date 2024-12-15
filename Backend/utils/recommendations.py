635c6efa7f59d9afce08980c0629cac3b97cdd4e
akinara666 <maksim040903@mail.ru>
2024-12-15 03:47:48 +0300

    refactor: deleted unused Backend folder

--- Backend/utils/recommendations.py
+++
@@ -1,6 +0,0 @@
-import random
-from models import Movie
-
-def get_recommendations(user_id):
-    all_movie_ids = [movie.id for movie in Movie.query.all()]
-    return random.sample(all_movie_ids, min(len(all_movie_ids), 2))
